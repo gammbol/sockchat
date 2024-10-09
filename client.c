8e120e779885c18ff34cdceeaf09767069245f06
Leonid Ushakov <tryin2think@t2t.local>
2024-10-09 20:44:10 +0300

    update client.c

--- client.c
+++ client.c
@@ -23,43 +23,34 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+void hintsInit(struct addrinfo *hints, size_t hintssize)
 {
-    int sockfd, numbytes;  
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
+    memset(hints, 0, hintssize);
+    hints->ai_family = AF_UNSPEC;
+    hints->ai_socktype = SOCK_STREAM;
+}
+
+void sockchatConnect(int *sockfd, struct addrinfo *hints, char *remote)
+{
+    int status;
+    struct addrinfo *servinfo, *p;
     char s[INET6_ADDRSTRLEN];
 
-    char buf[MAXDATASIZE] = "Hi there!";
-
-
-    if (argc < 2) {
-        fprintf(stderr,"usage: client hostname [message-to-send]\n");
-        exit(1);
-    }
-
-    if (argc == 3) 
-      strcpy(buf, argv[2]);
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+    if ((status = getaddrinfo(remote, PORT, hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return;
     }
 
     // loop through all the results and connect to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+        if ((*sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
             perror("client: socket");
             continue;
         }
 
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
+        if (connect(*sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(*sockfd);
             perror("client: connect");
             continue;
         }
@@ -69,14 +60,35 @@ int main(int argc, char *argv[])
 
     if (p == NULL) {
         fprintf(stderr, "client: failed to connect\n");
-        return 2;
+        return;
     }
 
     inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
             s, sizeof s);
     printf("client: connecting to %s\n", s);
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo);
+}
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd, numbytes;  
+    struct addrinfo hints;
+    char buf[MAXDATASIZE] = "Hi there!";
+
+
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr,"usage: client hostname [message-to-send]\n");
+        exit(1);
+    }
+
+    if (argc == 3) 
+      strcpy(buf, argv[2]);
+
+    hintsInit(&hints, sizeof hints);
+    sockchatConnect(&sockfd, &hints, argv[1]);
+
 
     if ((numbytes = send(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
         perror("send");
